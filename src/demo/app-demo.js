62e61630e47439bec9d9a9ab10f99e26b204290a
uoowuo <uoowuo@zoho.com>
2016-08-24 01:08:37 +0300

    Adjustment for vertical screens

--- src/demo/app-demo.js
+++ src/demo/app-demo.js
@@ -1,18 +1,28 @@
-const scene = require('../core/scene.js');
-const __s__ = require('../utils/js-helpers.js');
-const __d__ = require('../utils/dom-utilities.js');
+(function (globals) {
+    'use strict';
 
-const node = document.getElementById("app-3d"),
-    t = new scene.Scene(
-        node,
+    const scene = require('../core/scene.js');
+    const __s__ = require('../utils/js-helpers.js');
+    const __d__ = require('../utils/dom-utilities.js');
+
+    // Get viewport element and create the scene in it, scaling it down on vertical screens
+    const viewElement = document.getElementById("app-3d");
+    var scaleFactor;
+    if(viewElement.clientWidth / viewElement.clientHeight < 1.2) {
+        scaleFactor = viewElement.clientWidth / viewElement.clientHeight;
+    } else {
+        scaleFactor = 1;
+    }
+    const t = new scene.Scene(
+        viewElement,
         {
             models: [
                 {
                     fileObj: "Ring.obj",
                     fileMtl: "Ring.mtl",
                     filesDir: "./3dobjects/",
-                    scale: {x: 0.22, y: 0.22, z: 0.22 },
-                    position: {x: -0.5, y: -1.3, z: 0 },
+                    scale: {x: 0.22 * scaleFactor, y: 0.22 * scaleFactor, z: 0.22 * scaleFactor },
+                    position: {x: -0.5 * scaleFactor, y: -1.4 * scaleFactor, z: 0 },
                     rotation: {x: 0, y: Math.PI / 3, z: 0 },
                     texturesMap:  {
                         "Big_Ring": new THREE.MeshPhongMaterial({ color: 0xd3b371, shininess: 100 }),
@@ -23,10 +33,12 @@ const node = document.getElementById("app-3d"),
         }
     );
 
-// console.log(t);
+    // console.log(t);
 
-__d__.addEventLnr(node, "modelLoaded", function(ev) {
+    __d__.addEventLnr(viewElement, "modelLoaded", function(ev) {
 
-    console.log(ev);
+        console.log(ev);
 
-});
+    });
+
+}(self));
